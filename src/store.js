3d26974e6653765fadd1f6e461d97189ec3858c5
pusp203 <Pusp203>
2022-02-09 13:17:38 +0545

    all products reducers created

--- src/store.js
+++ src/store.js
@@ -2,9 +2,29 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const reducer = combineReducers({
+import { productsReducer } from './reducers/productReducers'
 
-})    
+const reducer = combineReducers({
+    products:productsReducer,
+    // productDetails: productDetailsReducer,
+    // newProduct: newProductReducer,
+    // product: productReducer,
+    // productReviews: productReviewsReducer,
+    // review: reviewReducer,
+    // auth: authReducer,
+    // user: userReducer,
+    // allUsers: allUsersReducer,
+    // userDetails: userDetailsReducer,
+    // forgotPassword: forgotPasswordReducer,
+    // cart: cartReducer,
+    // newOrder: newOrderReducer,
+    // myOrders: myOrdersReducer,
+    // allOrders: allOrdersReducer,
+    // orderDetails: orderDetailsReducer,
+    // order: orderReducer,
+    // newReview: newReviewReducer
+})
+  
 
 let initialState = {
 }
