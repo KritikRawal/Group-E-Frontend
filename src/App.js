3d80b0b0ac52c12f7205e0f6fae19767e4445816
pusp203 <Pusp203>
2022-02-09 08:06:44 +0545

    Done with the Home Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/layouts/Header"
 import Footer from "./components/layouts/footer";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <Home />
     </div>
   );
 }
