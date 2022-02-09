fa6388c06b739151ce6f577a0a14ec5d17904f69
pusp203 <Pusp203>
2022-02-09 07:19:00 +0545

    css is added to the header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/layouts/Header"
+import Footer from "./components/layouts/footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
