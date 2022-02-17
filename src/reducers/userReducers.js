8e7e0e197b3a2c3e502b495f34911086011a7e26
pusp203 <Pusp203>
2022-02-17 17:09:10 +0545

    register user reducer and action

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -2,9 +2,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    // REGISTER_USER_REQUEST,
-    // REGISTER_USER_SUCCESS,
-    // REGISTER_USER_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
     // LOAD_USER_REQUEST,
     // LOAD_USER_SUCCESS,
     // LOAD_USER_FAIL,
@@ -45,7 +45,7 @@ export const authReducer = (state = { user: {} }, action) => {
     switch (action.type) {
 
         case LOGIN_REQUEST:
-        // case REGISTER_USER_REQUEST:
+        case REGISTER_USER_REQUEST:
         // case LOAD_USER_REQUEST:
             return {
                 loading: true,
@@ -53,7 +53,7 @@ export const authReducer = (state = { user: {} }, action) => {
             }
 
         case LOGIN_SUCCESS:
-        // case REGISTER_USER_SUCCESS:
+        case REGISTER_USER_SUCCESS:
         // case LOAD_USER_SUCCESS:
             return {
                 ...state,
@@ -84,7 +84,7 @@ export const authReducer = (state = { user: {} }, action) => {
         //     }
 
         case LOGIN_FAIL:
-        // case REGISTER_USER_FAIL:
+        case REGISTER_USER_FAIL:
             return {
                 ...state,
                 loading: false,
