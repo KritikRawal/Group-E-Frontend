f7ab4c2478878b4ff1646819bf8b32fc31832d0d
pusp203 <Pusp203>
2022-02-19 22:00:00 +0545

    implemented protected route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Login from './components/user/Login';
 import Register from './components/user/Register';
 import Profile  from './components/user/Profile';
 
+import ProtectedRoute from './components/route/ProtectedRoute';
 import { loadUser } from './actions/userActions'
 import store from './store'
 function App() {
@@ -32,7 +33,7 @@ function App() {
 
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/me" component={Profile} exact/>
+        <ProtectedRoute path="/me" component={Profile} exact/>
 
      
       
