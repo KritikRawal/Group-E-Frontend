af2cc85a3d0a2683ee367ade2240803b716773c7
pusp203 <Pusp203>
2022-02-21 22:45:20 +0545

    handle shipping info

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { ADD_TO_CART,REMOVE_ITEM_CART } from '../constants/cartConstants'
+import { ADD_TO_CART,REMOVE_ITEM_CART,SAVE_SHIPPING_INFO } from '../constants/cartConstants'
 
 export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/v1/product/${id}`)
@@ -30,13 +30,13 @@ export const removeItemFromCart = (id) => async (dispatch, getState) => {
 
 }
 
-// export const saveShippingInfo = (data) => async (dispatch) => {
+export const saveShippingInfo = (data) => async (dispatch) => {
 
-//     dispatch({
-//         type: SAVE_SHIPPING_INFO,
-//         payload: data
-//     })
+    dispatch({
+        type: SAVE_SHIPPING_INFO,
+        payload: data
+    })
 
-//    localStorage.setItem('shippingInfo', JSON.stringify(data))
+   localStorage.setItem('shippingInfo', JSON.stringify(data))
 
-// }
+}
