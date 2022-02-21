5bd21a81024134faaa7d891c170457b1bc3c90d3
pusp203 <Pusp203>
2022-02-21 22:14:49 +0545

    cart item deletion working

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from '../constants/cartConstants'
+import { ADD_TO_CART,REMOVE_ITEM_CART } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [], shippingInfo: {} }, action) => {
     switch (action.type) {
@@ -20,11 +20,11 @@ export const cartReducer = (state = { cartItems: [], shippingInfo: {} }, action)
                 }
             }
 
-        // case REMOVE_ITEM_CART:
-        //     return {
-        //         ...state,
-        //         cartItems: state.cartItems.filter(i => i.product !== action.payload)
-        //     }
+        case REMOVE_ITEM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(i => i.product !== action.payload)
+            }
 
 
         // case SAVE_SHIPPING_INFO:
